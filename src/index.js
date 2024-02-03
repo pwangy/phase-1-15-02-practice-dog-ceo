c1f7e8e66d50b41bfb4837d34f778ba8483acada
pwangy <blipsandclicks@gmail.com>
2024-02-02 20:27:44 -0800

    challenge 1 & 2 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('%c HI', 'color: firebrick')
+const imageContainer = document.querySelector('#dog-image-container')
+const breedsUl = document.getElementById('dog-breeds')
+
+const renderImage = (imageUrl) => {
+	const image = document.createElement('img')
+	image.setAttribute('src', imageUrl) // image.src = imageUrl also works
+	image.setAttribute('alt', 'dog image')
+	image.setAttribute('class', 'dog-image')
+	imageContainer.appendChild(image)
+}
+
+const fetchImages = () => {
+	const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'
+
+	fetch(imgUrl)
+	.then(res => res.json())
+	.then(imgData => imgData.message.forEach(url => renderImage(url)))
+	.catch(err => console.error(err))
+}
+
+const listBreed = (breedName) => {
+	const createLi = document.createElement('li')
+	createLi.innerText = breedName
+	breedsUl.appendChild(createLi)
+}
+
+const fetchBreeds = () => {
+	const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+	fetch(breedUrl)
+    .then(res => res.json())
+    .then(breedObj => {
+        const breedsArray = Object.keys(breedObj.message)
+        breedsArray.forEach((breed) => listBreed(breed))
+    })
+    .catch(err => console.error(err))
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	fetchImages()
+    fetchBreeds()
+})
